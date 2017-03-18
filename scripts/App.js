acfa1ee3dedb73b206f6b815a69131f164fbabf2
ozzoozzo <ozzo.ozzo.ozzo@gmail.com>
2017-03-18 23:18:51 +0100

    adjust editorconfig, reformat code

--- scripts/App.js
+++ scripts/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
-    return (
-      // Add your component markup and other subcomponent references here.
-      <h1>Hello, World!</h1>
-    );
-  }
+	render() {
+		return (
+			// Add your component markup and other subcomponent references here.
+			<h1>Hello, World!</h1>
+			<Form />
+		);
+	}
 }
