731842654bd85bf3301b17770947be2f44a6f484
ozzoozzo <ozzo.ozzo.ozzo@gmail.com>
2017-03-20 02:05:38 +0100

    render multiple input elements

--- scripts/Form.jsx
+++ scripts/Form.jsx
@@ -1,15 +1,24 @@
+require('./Form.less');
 import React, { Component } from 'react';
 import Input from './Input';
 
 class Form extends Component {
+
+	renderInputs = () => {
+		var inputs = [];
+		for (var i = 0; i < 10; i++) inputs.push(<div key={i} className="gap"><Input value="" /></div>);
+		return inputs;
+	};
+
 	render() {
 		return (
 			<div>
 				<h2>Form</h2>
-				<Input value="" />
+				{this.renderInputs()}
 			</div>
 		);
 	}
+
 }
 
 export default Form;
