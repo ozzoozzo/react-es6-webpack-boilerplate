0bcac777abebf28f20ff9bd62494759539b6e9a5
ozzoozzo <ozzo.ozzo.ozzo@gmail.com>
2017-03-30 08:14:34 +0200

    use tabs for indentation

--- scripts/InputNew.jsx
+++ scripts/InputNew.jsx
@@ -3,71 +3,71 @@ import React, { PureComponent, PropTypes } from 'react';
 
 class InputNew extends PureComponent {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: props.value || '',
-        };
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: props.value || '',
+		};
+	}
 
-    handleChange = (event) => {
-        this.setState({
-            value: event.target.value,
-        });
-    };
+	handleChange = (event) => {
+		this.setState({
+			value: event.target.value,
+		});
+	};
 
-    handleBlur = (event) => {
-        this.props.onChange(event.target.name, event.target.value);
-    };
+	handleBlur = (event) => {
+		this.props.onChange(event.target.name, event.target.value);
+	};
 
-    renderWarningMsg(warning, width) {
-        if (!warning) return null;
-        const warnings = (warning instanceof Array) ? warning : [warning];
-        const warningWidthStyle = width ? { width: width + 'px' } : {};
-        return (
-            <ul style={warningWidthStyle}>
-                {warnings.map((warningMsg, index) => <li key={index}>{warningMsg}</li>)}
-            </ul>
-        );
-    }
+	renderWarningMsg(warning, width) {
+		if (!warning) return null;
+		const warnings = (warning instanceof Array) ? warning : [warning];
+		const warningWidthStyle = width ? { width: width + 'px' } : {};
+		return (
+			<ul style={warningWidthStyle}>
+				{warnings.map((warningMsg, index) => <li key={index}>{warningMsg}</li>)}
+			</ul>
+		);
+	}
 
-    render() {
-        const { name, placeholder, label, required, disabled, maxLength, width, warning } = this.props;
-        const { value } = this.state;
-        const containerClassName = `container ${disabled ? 'disabled' : ''} ${warning ? 'warning' : ''}`;
-        const labelClassName = `${value || placeholder ? 'top' : ''} ${required ? 'required' : ''}`;
-        const inputWidthStyle = width ? { width: width + 'px' } : {};
-        const labelWidthStyle = width ? { width: (width - 18 - (warning && !(value || placeholder) ? 25 : 0)) + 'px' } : {};
-        return (
-            <span class={containerClassName}>
-                <input
-                    type="text"
-                    name={name}
-                    placeholder={placeholder}
-                    value={value}
-                    disabled={disabled}
-                    maxLength={maxLength}
-                    style={inputWidthStyle}
-                    onChange={this.handleChange}
-                    onBlur={this.handleBlur} />
-                <label class={labelClassName} style={labelWidthStyle}>{label}</label>
-                {this.renderWarningMsg(warning, width)}
-            </span>
-        );
-    }
+	render() {
+		const { name, placeholder, label, required, disabled, maxLength, width, warning } = this.props;
+		const { value } = this.state;
+		const containerClassName = `container ${disabled ? 'disabled' : ''} ${warning ? 'warning' : ''}`;
+		const labelClassName = `${value || placeholder ? 'top' : ''} ${required ? 'required' : ''}`;
+		const inputWidthStyle = width ? { width: width + 'px' } : {};
+		const labelWidthStyle = width ? { width: (width - 18 - (warning && !(value || placeholder) ? 25 : 0)) + 'px' } : {};
+		return (
+			<span class={containerClassName}>
+				<input
+					type="text"
+					name={name}
+					placeholder={placeholder}
+					value={value}
+					disabled={disabled}
+					maxLength={maxLength}
+					style={inputWidthStyle}
+					onChange={this.handleChange}
+					onBlur={this.handleBlur} />
+				<label class={labelClassName} style={labelWidthStyle}>{label}</label>
+				{this.renderWarningMsg(warning, width)}
+			</span>
+		);
+	}
 }
 
 InputNew.propTypes = {
-    label: PropTypes.string,
-    name: PropTypes.string,
-    placeholder: PropTypes.string,
-    value: PropTypes.string,
-    required: PropTypes.bool,
-    disabled: PropTypes.bool,
-    maxLength: PropTypes.number,
-    width: PropTypes.number,
-    onChange: PropTypes.func.isRequired,
-    warning: PropTypes.any,
+	label: PropTypes.string,
+	name: PropTypes.string,
+	placeholder: PropTypes.string,
+	value: PropTypes.string,
+	required: PropTypes.bool,
+	disabled: PropTypes.bool,
+	maxLength: PropTypes.number,
+	width: PropTypes.number,
+	onChange: PropTypes.func.isRequired,
+	warning: PropTypes.any,
 };
 
 InputNew.defaultProps = {};
